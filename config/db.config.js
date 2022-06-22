b4d8fa62ca0c1e42aeebf0367487817b404bd0a7
Anubhav Dixit <anubhavdxt46@gmail.com>
2022-06-22 14:20:48 +0530

    Update MySQL DB configuration

--- config/db.config.js
+++ config/db.config.js
@@ -2,10 +2,10 @@
 const mysql = require("mysql");
 //local mysql db connection
 const dbConn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "@nubhav143744AD",
-  database: "emp-reg-app",
+  host: env(DB_HOST, "remotemysql.com"),
+  user: env(DB_USERNAME, "58qBb1YrN2"),
+  password: env(DB_PASSWORD, "V9339TG5LU"),
+  database: env(DB_DATABASE, "58qBb1YrN2"),
 });
 dbConn.connect(function (err) {
   if (err) throw err;
